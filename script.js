2c779bfcea4f9e382a07c88e95ce8fc5cfafe81d
Reena Thomas <rtthomas@ucsc.edu>
2024-11-14 10:35:15 -0800

    egypt

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ $(document).ready(function() {
   var maxSliderValue = 6; // Maximum slider value
 
   function showImage(index) {
+    // Remove 'active' class from all images
     $('.time-period-image').removeClass('active');
+    
+    // Add 'active' class to the image corresponding to the current slider value
     $('.time-period-image').eq(index).addClass('active');
   }
 
@@ -16,36 +19,42 @@ $(document).ready(function() {
     }
   }
 
+  // Click event for each time period link to navigate
   $('.time-period-link').on('click', function(e) {
-    e.preventDefault();
-    var value = parseInt($(this).attr('data-value'));
-    navigateToPage(value);
+    e.preventDefault(); // Prevent default link action
+    var value = parseInt($(this).attr('data-value')); // Get the data-value (index) from the clicked link
+    navigateToPage(value); // Navigate to the corresponding time period page
   });
 
+  // Update the image when slider is moved
   $('#time-period-slider').on('input', function() {
     var value = parseInt($(this).val());
     sliderValue = value;
-    showImage(value);
+    showImage(value); // Show the image corresponding to the slider value
   });
 
+  // Automatically move the slider and update images every 3 seconds
   function updateSlider() {
-    sliderValue = (sliderValue + 1) % (maxSliderValue + 1);
-    $('#time-period-slider').val(sliderValue);
-    showImage(sliderValue);
+    sliderValue = (sliderValue + 1) % (maxSliderValue + 1); // Increment and loop back
+    $('#time-period-slider').val(sliderValue); // Update slider value
+    showImage(sliderValue); // Show the next image
   }
 
+  // Set an interval for automatic slider updates
   var intervalDuration = 3000; // Duration between automatic slider updates (in milliseconds)
   var intervalId = setInterval(updateSlider, intervalDuration);
 
+  // Clicking on an image will stop the automatic interval and navigate to the corresponding time period page
   $('.time-period-image').on('click', function() {
-    clearInterval(intervalId);
-    var value = parseInt($(this).parent().attr('data-value'));
-    navigateToPage(value);
+    clearInterval(intervalId); // Stop the automatic slider update
+    var value = parseInt($(this).parent().attr('data-value')); // Get the data-value from the image's parent link
+    navigateToPage(value); // Navigate to the corresponding time period page
   });
 
   showImage(sliderValue); // Display the first image initially
 });
 
+// Function to return the corresponding link for each time period index
 function getTimePeriodLink(value) {
   var linkUrls = {
     0: 'ancient-egypt.html',
@@ -55,7 +64,6 @@ function getTimePeriodLink(value) {
     4: 'industrial-revolution.html',
     5: 'world-wars.html',
     6: 'modern-era.html'
-    // Add more time periods with their respective links
   };
   return linkUrls[value];
 }
